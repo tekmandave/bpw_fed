cd6344481856964e96f2b4dbd9f459cb43d044aa
Dave Pomeroy <rwaters44@gmail.com>
2021-04-10 20:50:16 -0500

    decent save point. Messy. Get used to it until MVP launch.

--- src/public/index.php
+++ src/public/index.php
@@ -1,13 +1,36 @@
 <?php
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+$config['api']['base'] = 'http://localhost:1337/';
+
+/* APP INIT */
+$app = new \Slim\App(['settings' => $config]);
+
+/* DIC */
+$container = $app->getContainer();
+$container['view'] = new \Slim\Views\PhpRenderer('../templates/');
+$container['api'] = function ($c) {
+    $api = $c['settings']['api'];
+    return $api;
+};
+/* ROUTES */
+$app->get('/{slug}', function (Request $request, Response $response, array $args) {
+    $slug = $args['slug'];
+
+    /* fetch the profile data */
+    $fetch = json_decode(file_get_contents($this->api['base'] . 'profiles/?slug=' . $slug));
+    $profile = $fetch[0];
+    /* set the template */
+    $template = 'default.phtml';
+    if($profile->template){$template = $profile->template . '.phtml';}
+    /* render */
+    $response = $this->view->render($response, $template, ['profile' => $profile]);
 
     return $response;
 });
